5acbeb47cb0a89429a2098b9b595aa247d460a51
Israel Comazzetto dos Reis <49065131+z3xddd@users.noreply.github.com>
2021-05-10 13:59:17 -0300

    Update osint_scanner.py

--- osint_scanner.py
+++ osint_scanner.py
@@ -3,24 +3,28 @@
 # OsInT Sc4N3r - Tool for automated recon process on bug bounty
 # by: Israel Comazzetto dos Reis [@z3xddd]
 
-import os
+from os import popen
 from time import sleep
 
 class OsInT_Sc4N3r(object):
-    def __init__(self, domain):
+    def __init__(self, domain, logs):
         self.domain = domain
+        self.logs = logs
     
     def enumerate_subdomains(self):
         print("[*] Enumeration subdomains process starting... [*]")
-        enumerate_command = 'echo '+self.domain+' | haktrails subdomains | notify'
-        print("[*] Haktrails execute process starting... [*]")
-        sleep(2)
-        print(os.popen(enumerate_command).read())
-        print("[+] Haktrails scan finished... See details on haktrails_result.txt [+]")
-        '''log_haktrails = open('/root/')
-                                file_n_err = open('n_err.txt','w')
-                        file_n_err.write(arquiv_n_errado)
-                        file_n_err.close()'''
+        if self.logs == 'Y' or 'y':
+            enumerate_command = 'echo '+self.domain+' | haktrails subdomains >> results/result_haktrails_'+self.domain+'.txt'
+            print("[*] Haktrails execute process starting... [*]")
+            sleep(2)
+            popen(enumerate_command)
+            print('[+] Haktrails scan finished...')
+        else:
+            enumerate_command = 'echo '+self.domain+' | haktrails subdomains'
+            print("[*] Haktrails execute process starting... [*]")
+            sleep(2)
+            popen(enumerate_command)
+            print('[+] Haktrails scan finished...')
 
 print("""\
 
@@ -37,6 +41,7 @@ print("""\
                                                                    Tool for automated recon process on Bug Bounty
                                                                                                       by: @z3xddd
     """)
-user_domain_input = str(input("[+] Enter domain to scan >>  "))
-domain_to_scan = OsInT_Sc4N3r(user_domain_input)
+user_domain_input = str(input("[+] Enter domain to scan >>  [ EX: domain.com.br ]  "))
+user_log_input = str(input("[+] Store the output in a text file? [ Write (Y) / (y) or (N) / (n) ] >>  "))
+domain_to_scan = OsInT_Sc4N3r(user_domain_input, user_log_input)
 domain_to_scan.enumerate_subdomains()
