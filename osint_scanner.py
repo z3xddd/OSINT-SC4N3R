ded1b47aac24c14b389484ee887291e71a21b3f1
Israel Comazzetto dos Reis <49065131+z3xddd@users.noreply.github.com>
2021-05-18 11:51:09 -0300

    Update osint_scanner.py

--- osint_scanner.py
+++ osint_scanner.py
@@ -17,12 +17,6 @@ class OsInT_Sc4N3r(object):
         else:
             pass
 
-    def portscan(self):
-        portscan_command = 'nmap -v -Pn -O -sV -p- '+self.domain+' > results/result_portscan_'+self.domain+'.txt'
-        print("[*] Portscan execute process starting... [*]")
-        print(popen(portscan_command).read())
-        print('[+] Portscan scan finished... See details on results/result_portscan_'+self.domain+'.txt [+]')
-
     def enumerate_subdomains_assetfinder(self):
         enumerate_command = 'assetfinder -subs-only '+self.domain+' >> results/result_assetfinder_'+self.domain+'.txt'
         print("[*] Assetfinder execute process starting... [*]")
@@ -34,7 +28,13 @@ class OsInT_Sc4N3r(object):
         print("[*] Httpx execute process starting... [*]")
         print(popen(enumerate_command).read())
         print('[+] Httpx scan finished... See details on results/result_httpx_'+self.domain+'.txt [+]')
-   
+
+    def portscan(self):
+        portscan_command = 'nmap -v -Pn -O -sV -p- -iL results/result_assetfinder_'+self.domain+'.txt > results/result_portscan_'+self.domain+'.txt'
+        print("[*] Portscan execute process starting... [*]")
+        print(popen(portscan_command).read())
+        print('[+] Portscan scan finished... See details on results/result_portscan_'+self.domain+'.txt [+]')   
+        
     def nuclei_attack(self):
         attack_command = 'nuclei -l results/result_httpx_'+self.domain+'.txt -t ../nuclei-templates/ > results/result_nuclei_'+self.domain+'.txt'
         print("[*] Nuclei attack execute process starting... [*]")
@@ -59,7 +59,7 @@ print("""\
 user_domain_input = str(input("[+] Enter domain to scan >>  [ EX: domain.com.br ]  "))
 domain_to_scan = OsInT_Sc4N3r(user_domain_input)
 domain_to_scan.validate_run_as_root()
-domain_to_scan.portscan()
 domain_to_scan.enumerate_subdomains_assetfinder()
+domain_to_scan.portscan()
 domain_to_scan.enumerate_webservers()
 domain_to_scan.nuclei_attack()
